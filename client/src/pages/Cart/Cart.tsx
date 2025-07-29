b798ecf49a46f8b596100b559b1b33aa00d7e640
Flavia <fernandezoliveraflavia@gmail.com>
2025-07-29 16:50:25 +0200

    fix: add basic Cart page to fix Vercel build

---
+++ client/src/pages/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartPage: React.FC = () => {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-4">Your Cart</h1>
+    </div>
+  );
+};
+
+export default CartPage;
