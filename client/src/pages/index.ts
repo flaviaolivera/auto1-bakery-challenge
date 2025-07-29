407fbf6bec3d2c432c2691e24b75d291ee9f3c53
Flavia <fernandezoliveraflavia@gmail.com>
2025-07-29 17:12:43 +0200

    fix: delete export pages to fix Vercel build

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,2 @@
 export { default as Home } from './Home/Home';
 export { default as Cart } from './Cart/Cart';
-export { default as Checkout } from './Checkout/Checkout';
-export { default as Confirmation } from './Confirmation/Confirmation';
